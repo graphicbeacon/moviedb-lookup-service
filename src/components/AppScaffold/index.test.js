048bec200bb4723ee9b7dd6ebc6a1f45940e45e8
graphicbeacon <jeropp00@gmail.com>
2019-05-12 19:14:46 +0100

    Refactors test with snapshot test.

--- src/components/AppScaffold/index.test.js
+++ src/components/AppScaffold/index.test.js
@@ -2,10 +2,15 @@ import React from 'react';
 import TestRenderer from 'react-test-renderer';
 import AppScaffold from './index';
 
-describe('<App />', () => {
+describe('<AppScaffold />', () => {
   test('should render without throwing', () => {
     const app = TestRenderer.create(<AppScaffold />);
     const appInstance = app.root;
     expect(appInstance.type).toBe(AppScaffold);
+  });
+
+  test('should match generated snapshot', () => {
+    const tree = TestRenderer.create(<AppScaffold />).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
